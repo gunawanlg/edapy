efd852911e19339527831c5acb55573080894ff8
Gunawan Gaol (ID) <gunawan.gaol@homecredit.co.id>
2019-10-29 19:07:36 +0700

    add docstring

--- edapy/plotting.py
+++ edapy/plotting.py
@@ -4,63 +4,101 @@ import math
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def distplot_numerical(data, cols_num, col_target=None):
-    if col_target is None:
-        n = math.ceil(len(cols_num) / 3)
-        fig, ax = plt.subplots(n, 3, figsize=(15, 3.5*n))
-        for col, a in zip(cols_num, ax.reshape(-1)):
+def distplot_numerical(data, cols_num, col_target=None, grid_c=3, w=15, h_factor=3.5):
+    """
+    Distplot numerical column attributes in small multiple grid.
+
+    Arguments:
+        data {pd.DataFrame} -- dataframe without infinite values
+        cols_num [str] -- numerical column in dataframe
+
+    Keyword Arguments:
+        col_target {str} -- if None, plot distribution without col_target grouping
+                             else plot cols_num attribute depending on type of col_target unique values,
+                             same as None if col_target unique values is not equal to 2
+        grid_c {int} -- default 3, number of column grid
+        w {int} -- default 15, figsize width arguments
+        h_factor {float} -- default 3.5, height of small plots
+    """
+    n = math.ceil(len(cols_num) / grid_c)
+    fig, ax = plt.subplots(n, grid_c, figsize=(w, h_factor*n))
+    if col_target is None:        
+        for col, a in zip(sorted(cols_num), ax.reshape(-1)):
             sns.distplot(data[col], ax=a)
             a.set_xlabel(col)
     else:
         if (data[col_target].nunique() == 2): # binary target
             target_mask = data[col_target] == sorted(data[col_target].unique())[0]
-            n = math.ceil(len(cols_num) / 3)
-            fig, ax = plt.subplots(n, 3, figsize=(15, 3.5*n))
-            for col, a in zip(cols_num, ax.reshape(-1)):
+            for col, a in zip(sorted(cols_num), ax.reshape(-1)):
                 sns.distplot(data[target_mask][col].dropna(), ax=a)                                    
                 sns.distplot(data[~target_mask][col].dropna(), ax=a)
                 a.legend(sorted(data[col_target].unique()))
                 a.set_xlabel(col)
         else: # regression target, plot only distribution of attributes
-            n = math.ceil(len(cols_num) / 3)
-            fig, ax = plt.subplots(n, 3, figsize=(15, 3.5*n))
-            for col, a in zip(cols_num, ax.reshape(-1)):
+            for col, a in zip(sorted(cols_num), ax.reshape(-1)):
                 sns.distplot(data[col], ax=a)
                 a.set_xlabel(col)
 
-def distplot_categorical(data, cols_cat, col_target=None, normalize=True):
+def distplot_categorical(data, cols_cat, col_target=None, normalize=True, grid_c=3, w=15, h_factor=3.5):
+    """
+    Distplot categorical column attributes in small multiple grid.
+
+    Arguments:
+        data {pd.DataFrame} -- dataframe without infinite values
+        cols_cat [str] -- categorical column in dataframe
+
+    Keyword Arguments:
+        col_target {str} -- if None, plot barchart using df.plot.bar without col_target grouping
+                             else plot cols_num attribute depending on type of col_target unique values,
+                             if binary col_target, plot barchart using df.plot.bar grouped by col_target,
+                             if numerical col_target, plot distplot using sns.displot grouped by col_target
+        normalize {Boolean} -- True to plot normalized values
+        grid_c {int} -- default 3, number of column grid
+        w {int} -- default 15, figsize width arguments
+        h_factor {float} -- default 3.5, height of small plots
+    """
+    n = math.ceil(len(cols_cat) / grid_c)
+    fig, ax = plt.subplots(n, grid_c, figsize=(w, h_factor*n))
     if col_target is None:
-        n = math.ceil(len(cols_cat) / 3)
-        fig, ax = plt.subplots(n, 3, figsize=(15, 3.5*n))
-        for col, a in zip(cols_cat, ax.reshape(-1)):
+        for col, a in zip(sorted(cols_cat), ax.reshape(-1)):
             data[col].value_counts(normalize=normalize).plot.bar(ax=a)
             a.set_xticklabels(a.get_xticklabels(), rotation=30, ha='right')
             a.set_xlabel(col)
         plt.tight_layout()
     else:        
         if (data[col_target].nunique() == 2): # binary target
-            n = math.ceil(len(cols_cat) / 3)
-            fig, ax = plt.subplots(n, 3, figsize=(15, 3.5*n))
-            for col, a in zip(cols_cat, ax.reshape(-1)):
+            for col, a in zip(sorted(cols_cat), ax.reshape(-1)):
                 data.groupby(col_target)[col].value_counts(normalize=normalize).unstack(0).plot.bar(ax=a)
                 a.set_xticklabels(a.get_xticklabels(), rotation=30, ha='right')
             plt.tight_layout()
         else: # regression target, distribution of target by each attributes                
-            n = math.ceil(len(cols_cat) / 3)
-            fig, ax = plt.subplots(n, 3, figsize=(15, 3.5*n))
-            for col, a in zip(cols_cat, ax.reshape(-1)):
+            for col, a in zip(sorted(cols_cat), ax.reshape(-1)):
                 for val in data[col].unique():
                     sns.distplot(data[data[col] == val][col_target], ax=a)
                 a.legend(data[col].unique())
                 a.set_xlabel(col)
 
-def diff_distplot_numerical(data, cols_num, col_target, filt_idx):
+def diff_distplot_numerical(data, cols_num, col_target, filt_idx, grid_c=3, w=15, h_factor=3.5):
+    """
+    Difference distplot numerical column attributes in small multiple grid.
+
+    Arguments:
+        data {pd.DataFrame} -- dataframe without infinite values
+        cols_num [str] -- numerical column in dataframe
+
+    Keyword Arguments:
+        col_target {str} -- if binary col_target, make 4 distplot combination of col_target and filt_idx unique values
+        filt_idx [Boolean] -- boolean list, used to filter data by using boolean indexing
+        grid_c {int} -- default 3, number of column grid
+        w {int} -- default 15, figsize width arguments
+        h_factor {float} -- default 3.5, height of small plots
+    """
+    n = math.ceil(len(cols_num) / grid_c)
+    fig, ax = plt.subplots(n, grid_c, figsize=(w, h_factor*n))
     if (data[col_target].nunique() == 2): # binary target
         sorted_keys = sorted(list(data[col_target].unique()))
         target_mask = data[col_target] == sorted_keys[0]
-        n = math.ceil(len(cols_num) / 3)
-        fig, ax = plt.subplots(n, 3, figsize=(15, 3.5*n))
-        for col, a in zip(cols_num, ax.reshape(-1)):
+        for col, a in zip(sorted(cols_num), ax.reshape(-1)):
             sns.distplot(data[target_mask][col].dropna(), ax=a)
             sns.distplot(data[~target_mask][col].dropna(), ax=a)
             sns.distplot(data[filt_idx & target_mask][col].dropna(), ax=a)
@@ -68,11 +106,25 @@ def diff_distplot_numerical(data, cols_num, col_target, filt_idx):
             a.legend(sorted_keys + ['filt_'+sorted_keys[0], 'filt_'+sorted_keys[1]])
             a.set_xlabel(col)
 
-def diff_distplot_categorical(data, cols_cat, col_target, filt_idx):
+def diff_distplot_categorical(data, cols_cat, col_target, filt_idx, grid_c=3, w=15, h_factor=3.5):
+    """
+    Difference distplot categorical column attributes in small multiple grid.
+
+    Arguments:
+        data {pd.DataFrame} -- dataframe without infinite values
+        cols_cat [str] -- numerical column in dataframe
+
+    Keyword Arguments:
+        col_target {str} -- if binary col_target, make diff barplot with negative and positive normalized value_counts
+        filt_idx [Boolean] -- boolean list, used to filter data by using boolean indexing
+        grid_c {int} -- default 3, number of column grid
+        w {int} -- default 15, figsize width arguments
+        h_factor {float} -- default 3.5, height of small plots
+    """
+    n = math.ceil(len(cols_cat) / grid_c)
+    fig, ax = plt.subplots(n, grid_c, figsize=(w, h_factor*n))
     if (data[col_target].nunique() == 2): # binary target
-        n = math.ceil(len(cols_cat) / 3)
-        fig, ax = plt.subplots(n, 3, figsize=(15, 3.5*n))
-        for col, a in zip(cols_cat, ax.reshape(-1)):
+        for col, a in zip(sorted(cols_cat), ax.reshape(-1)):
             data1 = data.groupby(col_target)[col].value_counts(normalize=True).unstack(0)
             data2 = data[filt_idx].groupby(col_target)[col].value_counts(normalize=True).unstack(0)
             (data2 - data1).plot.bar(ax=a)
